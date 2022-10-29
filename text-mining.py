e3894c82279c7c1ab91bed48de34f664a9c44085
lavenderliao <112440325+lavenderliao@users.noreply.github.com>
2022-10-29 16:31:20 -0400

    Assignment 2

--- text-mining.py
+++ text-mining.py
@@ -1,4 +1,8 @@
 from imdb import Cinemagoer
+import random
+import string
+import sys
+from unicodedata import category
 
 # create an instance of the Cinemagoer class
 ia = Cinemagoer()
@@ -11,12 +15,87 @@ print(movie.movieID)
 movie_reviews = ia.get_movie_reviews('14109724')
 first_comment = movie_reviews['data']['reviews'][0]['content']
 
+# word frequencies
+
+def process_file(filename):
+    """Makes a histogram that contains the words from a file.
+    filename: string
+    skip_header: boolean, whether to skip the Gutenberg header
+    returns: map from each word to the number of times it appears.
+    """
+    hist = {}
+
+    # strippables = string.punctuation + string.whitespace
+    # via: https://stackoverflow.com/questions/60983836/complete-set-of-punctuation-marks-for-python-not-just-ascii
+
+    strippables = ''.join(
+        [chr(i) for i in range(sys.maxunicode) if category(chr(i)).startswith("P")]
+    )
+
+    for word in filename.split():
+        # remove punctuation and convert to lowercase
+        word = word.strip(strippables)
+        word = word.lower()
+
+        # update the histogram
+        hist[word] = hist.get(word, 0) + 1
+
+    return hist
+
+def most_common(hist, excluding_stopwords=True):
+    """Makes a list of word-freq pairs(tuples) in descending order of frequency.
+    hist: map from word to frequency
+    excluding_stopwords: a boolean value. If it is True, do not include any stopwords in the list.
+    returns: list of (frequency, word) pairs
+    """
+    t = []
+
+    stopwords = process_file('data/stopwords.txt',False)
+
+    stopwords = list(stopwords.keys())
+    # print(stopwords)
+
+    for word, freq in hist.items():
+        if excluding_stopwords:
+            if word in stopwords:
+                continue
+
+        t.append((freq, word))
+
+    t.sort(reverse=True)
+    return t
+
+
+def print_most_common(hist, num=10):
+    """Prints the most commons words in a histgram and their frequencies.
+    hist: histogram (map from word to frequency)
+    num: number of words to print
+    """
+    t = most_common(hist)
+    print('The most common words are:')
+    for freq, word in t[:num]:
+        print(word, '\t', freq)
+
+def main():
+    hist = process_file(first_comment)
+    t = most_common(hist, False)
+
+    print('The most common words are:')
+    for freq, word in t[0:20]:
+        print(word, '\t', freq)
+    print_most_common(hist, 20)
+
+if __name__ == "__main__":
+    main()
+
 # sentiment analysis
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 sentence = first_comment
 score = SentimentIntensityAnalyzer().polarity_scores(sentence)
 print(score)
+
+# similarity of two comments
 
 # if __name__ == "__main__":
 #     main()
